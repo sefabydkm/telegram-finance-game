df895e4b6b7d2ed87d37a1c841007ec53dd25be7
sefabydkm <sefabuyukdikmen@gmail.com>
2023-10-04 21:05:32 +0300

    muidatatable added

--- src/components/StockList.js
+++ src/components/StockList.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import MUIDataTable from "mui-datatables";
 
 function StockList(props) {
+    const columns = [
+        {
+            name: "name",
+            label: "Name",
+            options: {
+                filter: true,
+                sort: true,
+            }
+        },
+        {
+            name: "company",
+            label: "Company",
+            options: {
+                filter: true,
+                sort: false,
+            }
+        },
+        {
+            name: "city",
+            label: "City",
+            options: {
+                filter: true,
+                sort: false,
+            }
+        },
+        {
+            name: "state",
+            label: "State",
+            options: {
+                filter: true,
+                sort: false,
+            }
+        },
+    ];
+
+    const data = [
+        { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+        { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+        { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+        { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
+    ];
+
+    const options = {
+
+        filterType: 'checkbox',
+        print:false,
+        download:false,
+        viewColumns:false,
+        selectableRows:'none',
+        onCellClick: (cellIndex, rowIndex) => {
+            console.log(cellIndex, rowIndex);
+        }
+    };
+
     return (
         <div>
-            StockList
+            <MUIDataTable
+                title={"Stock List"}
+                data={data}
+                columns={columns}
+                options={options}
+            />
         </div>
     );
 }
