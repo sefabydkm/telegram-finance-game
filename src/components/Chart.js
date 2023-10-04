395622dcc25fb174c773e146af3c18c2def7c35d
Batuhan Çalışkan <batuhanrap07@windowslive.com>
2023-10-04 21:15:29 +0300

    init

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import ApexCharts from 'apexcharts'
 
 function Charts(props) {
+    useEffect(() => {
+        const options = {
+            chart: {
+                type: 'candlestick',
+            },
+            series: [{
+                data: [
+                    [1538856000000, [6593.34, 6600, 6582.63, 6600]],
+                    [1538856100000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856200000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856300000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856400000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856500000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856600000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856700000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856800000, [6595.16, 6604.76, 6590.73, 6593.86]],
+                    [1538856900000, [6595.16, 6604.76, 6590.73, 6593.86]]
+                ]
+            }],
+            plotOptions: {
+                candlestick: {
+                    colors: {
+                        upward: '#3C90EB',
+                        downward: '#d91b37'
+                    }
+                }
+            }
+        };
+
+        const chart = new ApexCharts(document.querySelector('#chart'), options);
+        chart.render();
+
+        // Cleanup when the component unmounts
+        return () => {
+            chart.destroy();
+        };
+    }, []);
     return (
-        <div>
-            Charts
-        </div>
+        <div id="chart" />
     );
 }
 
