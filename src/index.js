46d5d18642c08640714b40aa8039b5d5d9ffba9d
sefabydkm <sefabuyukdikmen@gmail.com>
2023-10-08 14:08:04 +0300

    telegram sdk added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+<script src="https://telegram.org/js/telegram-web-app.js"></script>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
