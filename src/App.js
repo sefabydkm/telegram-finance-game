adf3e0269cde5aaf16f74db6cc0094b2dfac789f
sefabydkm <sefabuyukdikmen@gmail.com>
2023-10-08 15:32:40 +0300

    bug fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     return (
      <BrowserRouter >
          <Routes>
-             <Route path="/telegram-finance-game/" element={<Navigate to="/stock" />} />
+             <Route path="/telegram-finance-game/" element={<Navigate to="/telegram-finance-game/stock" />} />
              <Route path="/telegram-finance-game/stock" element={<StockListView />} />
              <Route path="/telegram-finance-game/stock/:id" element={<ChartView />} />
          </Routes>
