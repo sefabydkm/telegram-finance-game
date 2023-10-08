286cad80c400c53c46fe313f38b82abc5d62b170
Batuhan Çalışkan <batuhanrap07@windowslive.com>
2023-10-08 18:19:36 +0300

    deployment

--- src/services/FinancialDataService.js
+++ src/services/FinancialDataService.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import axios from "axios";
 
-function FinancialDataService(props) {
+let stocks = ["IBM", "AAPL", "MSFT", "AMZN", "TSLA"]
 
-    var stocks = ["IBM","AAPL","MSFT","AMZN","TSLA"]
-    function getStockList(){
-        return stocks.map((stock) =>{
-            return axios.get()
-        })
-    }
+function getStockList() {
+    return stocks.map((stock) => {
+        axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=ZQRUW4GP3NM4XKDO`)
+            .then(response => {
+                return response.data['Time Series (Daily)']
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    })
 }
 
-export default FinancialDataService;
+export default getStockList;
--- src/view/StockListView.js
+++ src/view/StockListView.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import StockList from "../components/StockList";
+import getStockList from "../services/FinancialDataService";
 
 function StockListView(props) {
+    console.log(getStockList())
     return (
         <div>
             <StockList></StockList>
