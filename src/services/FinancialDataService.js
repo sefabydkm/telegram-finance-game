9c717406528136d720db7312f1a3ebe03ef403c7
Batuhan Çalışkan <batuhanrap07@windowslive.com>
2023-10-08 18:40:44 +0300

    deployment

--- src/services/FinancialDataService.js
+++ src/services/FinancialDataService.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import axios from "axios";
+import amazonData from "../db/amazon.json"
+import appleData from "../db/apple.json"
+import ibmData from "../db/ibm.json"
+import microsoftData from "../db/microsoft.json"
+import teslaData from "../db/tesla.json"
 
-let stocks = ["IBM", "AAPL", "MSFT", "AMZN", "TSLA"]
+let stocks = ["ibm", "apple", "microsoft", "amazon", "tesla"]
+let stockList = []
 
-function getStockList() {
-    return stocks.map((stock) => {
-        axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&apikey=ZQRUW4GP3NM4XKDO`)
-            .then(response => {
-                return response.data['Time Series (Daily)']
-            })
-            .catch(error => {
-                console.error('Error:', error);
-            });
-    })
+function getStockList(stock) {
+
 }
 
 export default getStockList;
